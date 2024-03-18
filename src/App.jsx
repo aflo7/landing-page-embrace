6e95201df70864b818c73eb32b3d8b02bd8e86b7
Andres Flores <andresflo883@gmail.com>
2024-03-18 13:43:41 -0400

    added buton and dog photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
 // import { useState } from 'react'
-import './App.css'
-
-const navStyle = {
-  color: 'purple'
-}
-
-const topNavStyle = {
-  backgroundColor: 'rgb(250,241,228)',
-  display: 'flex',
-  justifyContent: 'flex-end',
-  gap: '40px'
-}
+import './App.css';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 export default function App() {
-
   return (
     <>
-      <div style={topNavStyle}>
+      <div className="top-nav">
         <div>Call Us: (800) 511-9182</div>
         <div>Contact Us</div>
         <div>MyEmbrace Login</div>
       </div>
-      <nav style={navStyle}>
-        <div>logo</div>
-        <div>
-          <div>Pet Insurance</div>
-          <div>Compare</div>
-          <div>Help Center</div>
-          <div>Resources</div>
-          <button className='get-quote-btn'>Get Quote</button>
+      <nav>
+        <RxHamburgerMenu className="burger-logo" />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <div className="embrace-text">embrace</div>
+          <div className="pet-insurance-text">PET INSURANCE</div>
         </div>
+        <button className="get-quote-btn">Get Quote</button>
       </nav>
+
+      <img
+        className="dog-img"
+        src="https://images.ctfassets.net/nx3pzsky0bc9/2sdUirqRQ4dXpJpVUcCKny/02edf394197f4f3df2e6f6fb6e2d2f54/Desktop_Work_at_Embrace.png?w=1600&fm=webp"
+      />
     </>
-  )
+  );
 }
